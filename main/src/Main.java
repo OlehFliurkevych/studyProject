868a8d762e51021fa5c675636ea069185bbd5f76
Oleh Fliurkevych <oleh0809@gmail.com>
2021-01-13 14:18:06 +0200

    Some updates

--- main/src/Main.java
+++ main/src/Main.java
@@ -6,8 +6,9 @@ public class Main {
   public static void main(String[] args) {
     Car car1 = new Car(1L, CarType.MINIVAN, "title1", 400);
     Car car2 = new Car(1L, CarType.SEDAN, "title2", 500);
+    Car car3 = new Car(1L, CarType.SPORTS, "title3", 200);
 
-    List<Car> cars = Arrays.asList(car1, car2);
+    List<Car> cars = Arrays.asList(car1, car2, car3);
 
     cars.forEach(car -> System.out.println(car));
   }
